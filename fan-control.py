ae2a1dc2251795626d593f505bac5be922c19fe6
Brian Gisseler <gissf1@gmail.com>
2026-04-28 03:50:24 -0500

    Consider max temp and cool down faster when needed

--- fan-control.py
+++ fan-control.py
@@ -350,10 +350,14 @@ while True:
 
 	# Average out temp values over the last 5 samples to smooth RPM changes and output our values
 	ZONE_A_TEMP_SAMPLES.append(PEAK_ZONE_A_TEMP); ZONE_A_TEMP_SAMPLES.pop(0)
-	FINAL_ZONE_A_TEMP = statistics.mean(ZONE_A_TEMP_SAMPLES)
+	AVG_ZONE_A_TEMP = statistics.mean(ZONE_A_TEMP_SAMPLES)
+	MAX_ZONE_A_TEMP = max(ZONE_A_TEMP_SAMPLES)
+	FINAL_ZONE_A_TEMP = (MAX_ZONE_A_TEMP + AVG_ZONE_A_TEMP) / 2
 	ZONE_B_TEMP_SAMPLES.append(PEAK_ZONE_B_TEMP); ZONE_B_TEMP_SAMPLES.pop(0)
-	FINAL_ZONE_B_TEMP = statistics.mean(ZONE_B_TEMP_SAMPLES)
-	sys.stdout.write("\nMaximum Zone A temp = " + str(PEAK_ZONE_A_TEMP) + "'C, averaged " + str(int(FINAL_ZONE_A_TEMP)) + "'C\nMaximum Zone B temp = " + str(PEAK_ZONE_B_TEMP) + "'C, averaged " + str(int(FINAL_ZONE_B_TEMP)) + "'C\n"); sys.stdout.flush()
+	AVG_ZONE_B_TEMP = statistics.mean(ZONE_B_TEMP_SAMPLES)
+	MAX_ZONE_B_TEMP = max(ZONE_B_TEMP_SAMPLES)
+	FINAL_ZONE_B_TEMP = (MAX_ZONE_B_TEMP + AVG_ZONE_B_TEMP) / 2
+	sys.stdout.write("\nMaximum Zone A temp = " + str(PEAK_ZONE_A_TEMP) + "'C, averaged " + str(int(AVG_ZONE_A_TEMP)) + "'C\nMaximum Zone B temp = " + str(PEAK_ZONE_B_TEMP) + "'C, averaged " + str(int(AVG_ZONE_B_TEMP)) + "'C\n"); sys.stdout.flush()
 
 	# Calculate our fan PWM values
 	if FAILED_FAN:
